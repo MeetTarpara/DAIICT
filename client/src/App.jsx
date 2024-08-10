9d166996703c492b2a4ed8d1979b477ab3f88be7
DavdaAbhi <abhidavda1303@gmail.com>
2024-08-10 22:27:37 +0530

    plant detect commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react'
 import Home from './pages/Home';
 import Signin from './pages/Signin';
@@ -8,28 +8,29 @@ import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
+import PlantDetect from './pages/PlantDetect';
 
 export default function App() {
   return (
     <>
-   
-    <BrowserRouter>
-    <Header/>
-      <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/sign-in' element={<Signin/>}></Route>
-        <Route path='/sign-up' element={<SignUp/>}></Route>
-        <Route path='/about' element={<About/>}></Route>
-        <Route element={<PrivateRoute />}>
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/create-listing' element={<CreateListing />} />
-        </Route>
 
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/sign-in' element={<Signin />}></Route>
+          <Route path='/sign-up' element={<SignUp />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/create-listing' element={<CreateListing />} />
+            <Route path='/plant-detect' element={<PlantDetect />} />
+          </Route>
+
+        </Routes>
+      </BrowserRouter>
     </>
-    
+
   )
 }
 
- 
